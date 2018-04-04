9f280b571134ed085723908b6712023c325af5eb
Unknown <ilana.tordjman@edu.ece.fr>
2018-04-04 14:07:58 +0200

    menu allegro+fichier

--- Allegro.cpp
+++ Allegro.cpp
@@ -7,26 +7,26 @@ int Menu()
     int fond;
 
     ///tant qu'on a pas cliqué sur un bouton
-    while((!mouse_b&1) && (mouse_x>300 && mouse_y<725))
+    if((mouse_b&1) && (mouse_x>300 && mouse_x<725))
     {
 
-        if(mouse_x>105 && mouse_y<210)
+        if(mouse_y>105 && mouse_y<210)
         {
             fond=1;
             return fond;
         }
-        if(mouse_x>280 && mouse_y<385)
+        if(mouse_y>280 && mouse_y<385)
         {
             fond=2;
             return fond;
         }
-        if(mouse_x>440 && mouse_y<545)
+        if(mouse_y>440 && mouse_y<545)
         {
             fond=3;
             return fond;
         }
         ///pour quitter le jeu
-        if(mouse_x>615 && mouse_y<720)
+        if(mouse_y>615 && mouse_y<720)
             exit(0);
     }
 }
--- main.cpp
+++ main.cpp
@@ -8,22 +8,30 @@ int main()
 {
     /// A appeler en 1er avant d'instancier des objets graphiques etc...
     grman::init();
+    BITMAP *fond;
+    fond =load_bitmap("pics/fond.bmp",NULL);
 
     /// Le nom du répertoire où se trouvent les images à charger
+    //
     grman::set_pictures_path("pics");
 
     /// Un exemple de graphe
     Graph g;
     g.make_example();
-
+    int choix=0;
 
     /// Vous gardez la main sur la "boucle de jeu"
     /// ( contrairement à des frameworks plus avancés )
     while ( !key[KEY_ESC] )
     {
-        int choix=0;
-        choix = Menu();
-        if(choix=1)
+
+        if(choix == 0)
+            {
+                choix = Menu();
+            blit(fond, grman::page, 0,0, 0, 0, 1024,768);
+        blit(grman::page, screen, 0,0, 0, 0, 1024,768);
+            }
+        if(choix ==1 )
         {
             /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
         g.update();
@@ -32,7 +40,7 @@ int main()
         grman::mettre_a_jour();
         }
 
-         if(choix=2)
+         if(choix==2)
         {
             /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
         g.update();
@@ -41,7 +49,7 @@ int main()
         grman::mettre_a_jour();
         }
 
-         if(choix=3)
+         if(choix==3)
         {
             /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
         g.update();
